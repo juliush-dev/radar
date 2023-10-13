d7820be2978a7a2e26c2fbbddf417e292e92f6e1
julius-dev <juliush.dev@gmail.com>
2023-10-13 12:11:51 +0200

    Group some routes by controller

--- routes/web.php
+++ routes/web.php
@@ -32,42 +32,117 @@ Route::middleware('splade')->group(function () {
         return view('welcome');
     })->name('welcome');
 
-    Route::get('/topics/learning-materials/{learningMaterial}/download', [App\Http\Controllers\TopicController::class, 'downloadLearningMaterial'])->name('topics.learning-materials.download');
+    Route::get(
+        '/topics/learning-materials/{learningMaterial}/download',
+        [App\Http\Controllers\TopicController::class, 'downloadLearningMaterial']
+    )->name('topics.learning-materials.download');
     Route::middleware('auth')->group(function () {
-        Route::prefix('topics')->name('topics.')->group(function () {
-            Route::post('/{topic}/learning-materials/upload', [App\Http\Controllers\TopicController::class, 'uploadLearningMaterial'])->name('learning-materials.upload');
-            Route::delete('/learning-materials/{learningMaterial}/remove', [App\Http\Controllers\TopicController::class, 'removeLearningMaterial'])->name('learning-materials.remove');
-            Route::post('/learning-materials/{learningMaterial}/publish', [App\Http\Controllers\TopicController::class, 'publishLearningMaterial'])->name('learning-materials.publish');
-            Route::post('/learning-materials/{learningMaterial}/unpublish', [App\Http\Controllers\TopicController::class, 'unpublishLearningMaterial'])->name('learning-materials.unpublish');
-            Route::post('/{topic}/publish', [App\Http\Controllers\TopicController::class, 'publishTopic'])->name('publish');
-            Route::post('/{topic}/unpublish', [App\Http\Controllers\TopicController::class, 'unpublishTopic'])->name('unpublish');
-            Route::post('/{topic}/apply-update', [App\Http\Controllers\TopicController::class, 'applyUpdate'])->name('apply-update');
+        Route::controller(App\Http\Controllers\TopicController::class)
+            ->prefix('topics')
+            ->name('topics.')
+            ->group(function () {
+                Route::post(
+                    '/{topic}/learning-materials/upload',
+                    'uploadLearningMaterial'
+                )->name('learning-materials.upload');
+                Route::delete(
+                    '/learning-materials/{learningMaterial}/remove',
+                    'removeLearningMaterial'
+                )->name('learning-materials.remove');
+                Route::post(
+                    '/learning-materials/{learningMaterial}/publish',
+                    'publishLearningMaterial'
+                )->name('learning-materials.publish');
+                Route::post(
+                    '/learning-materials/{learningMaterial}/unpublish',
+                    'unpublishLearningMaterial'
+                )->name('learning-materials.unpublish');
+                Route::post(
+                    '/{topic}/publish',
+                    'publishTopic'
+                )->name('publish');
+                Route::post(
+                    '/{topic}/unpublish',
+                    'unpublishTopic'
+                )->name('unpublish');
+                Route::post(
+                    '/{topic}/apply-update',
+                    'applyUpdate'
+                )->name('apply-update');
 
-            Route::get('/subjects/{subject}/edit', [App\Http\Controllers\TopicController::class, 'editSubject'])->name('subjects.edit');
-            Route::patch('/subjects/{subject}', [App\Http\Controllers\TopicController::class, 'updateSubject'])->name('subjects.update');
-        });
+                Route::get(
+                    '/subjects/{subject}/edit',
+                    'editSubject'
+                )->name('subjects.edit');
+                Route::patch(
+                    '/subjects/{subject}',
+                    'updateSubject'
+                )->name('subjects.update');
+            });
 
         Route::resource('topics', App\Http\Controllers\TopicController::class)->except(['show', 'index']);
         Route::resource('skills', App\Http\Controllers\SkillController::class)->except(['show', 'index']);
         Route::resource('profile', ProfileController::class)->except(['create', 'index', 'show']);
 
-        Route::post('/profile/{profile}/block', [App\Http\Controllers\ProfileController::class, 'block'])->name('profile.block');
-        Route::post('/profile/{profile}/unblock', [App\Http\Controllers\ProfileController::class, 'unblock'])->name('profile.unblock');
+        Route::controller(App\Http\Controllers\ProfileController::class)
+            ->prefix('profile')
+            ->name("profile.")
+            ->group(function () {
+                Route::post(
+                    '/{profile}/block',
+                    'block'
+                )->name('block');
+                Route::post(
+                    '/{profile}/unblock',
+                    'unblock'
+                )->name('unblock');
+            });
+
         Route::resource('fields', App\Http\Controllers\FieldController::class)->except(['index', 'show']);;
 
-        Route::prefix('skills')->name('skills.')->group(function () {
-            Route::post('/{skill}/assess', [App\Http\Controllers\SkillController::class, 'assess'])->name('assess');
-            Route::get('/groups/{group}/edit', [App\Http\Controllers\SkillController::class, 'editGroup'])->name('groups.edit');
-            Route::patch('/groups/{group}', [App\Http\Controllers\SkillController::class, 'updateGroup'])->name('groups.update');
+        Route::controller(App\Http\Controllers\SkillController::class)
+            ->prefix('skills')
+            ->name('skills.')
+            ->group(function () {
+                Route::post(
+                    '/{skill}/assess',
+                    'assess'
+                )->name('assess');
+                Route::get(
+                    '/groups/{group}/edit',
+                    'editGroup'
+                )->name('groups.edit');
+                Route::patch(
+                    '/groups/{group}',
+                    'updateGroup'
+                )->name('groups.update');
 
-            Route::get('/types/{type}/edit', [App\Http\Controllers\SkillController::class, 'editType'])->name('types.edit');
-            Route::patch('/types/{type}', [App\Http\Controllers\SkillController::class, 'updateType'])->name('types.update');
-        });
+                Route::get(
+                    '/types/{type}/edit',
+                    'editType'
+                )->name('types.edit');
+                Route::patch(
+                    '/types/{type}',
+                    'updateType'
+                )->name('types.update');
+            });
     });
-    Route::resource('fields', App\Http\Controllers\FieldController::class)->only(['index', 'show']);
-    Route::resource('topics', App\Http\Controllers\TopicController::class)->only(['index', 'show']);
-    Route::resource('skills', App\Http\Controllers\SkillController::class)->only(['index', 'show']);
+    Route::resource(
+        'fields',
+        App\Http\Controllers\FieldController::class
+    )->only(['index', 'show']);
+    Route::resource(
+        'topics',
+        App\Http\Controllers\TopicController::class
+    )->only(['index', 'show']);
+    Route::resource(
+        'skills',
+        App\Http\Controllers\SkillController::class
+    )->only(['index', 'show']);
 
-    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get(
+        '/dashboard',
+        [App\Http\Controllers\DashboardController::class, 'index']
+    )->name('dashboard.index');
     require __DIR__ . '/auth.php';
 });
