d5aa09e2813d647b987b732cae3443661dc4d959
julius-dev <juliush.dev@gmail.com>
2023-12-13 12:36:12 +0100

    Set notes to the default page after registration

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/topics';
+    public const HOME = '/notes';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
