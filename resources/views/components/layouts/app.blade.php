326527f2ae12bc27c0e7a93ad50925735024a00a
julius-dev <juliush.dev@gmail.com>
2023-12-16 11:45:01 +0100

    Avoid layout scrolling on x-axis

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,7 +1,7 @@
      <x-splade-data remember="radar" local-storage store="radar" default="{ dark: false, navigation: true }" />
      <div v-bind:class="radar.dark && 'dark'" class="h-screen w-screen flex items-center justify-center">
          <div
-             class="h-full w-full flex flex-col relative bg-slate-100 dark:text-white text-slate-600  dark:bg-slate-800  transition-all duration-100">
+             class="h-full w-full flex flex-col relative bg-slate-100 dark:text-white text-slate-600  dark:bg-slate-800  transition-all duration-100 overflow-hidden">
              <x-nav-bar :$activePage :$previousRoute :$icon />
              <div class="flex-1 w-full overflow-hidden overflow-y-auto" @preserveScroll('main-layout')>
                  <div class="w-full lg:w-[920px] mx-auto h-full">
