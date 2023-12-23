6051426cfbf92d7974e04dd32cf6d2cb45dfb19d
julius-dev <juliush.dev@gmail.com>
2023-12-23 20:49:38 +0100

    Make editor readonly on small screens

--- resources/views/components/note.blade.php
+++ resources/views/components/note.blade.php
@@ -7,6 +7,9 @@
         <div class="flex flex-wrap md:items-center gap-6 mb-8 w-full" id="{{ $note->id }}">
             <div class="font-medium text-slate-400 first-letter:uppercase dark:text-slate-600">
                 {{ $note->author->name }}
+                @if (Agent::isPhone())
+                    <span class="ml-8 mark-verb"><span class="px-2">Readonly</span></span>
+                @endif
             </div>
             @auth
                 @can('edit-note', [$note])
@@ -34,37 +37,43 @@
                 @endcan
             @endauth
         </div>
-        @can('edit-note', [$note])
-            <tip-tap v-model="form.content" note-id="@js($note->id)" :form="form" />
+        @if (Agent::isPhone())
+            @can('edit-note', [$note])
+                <div class="ProseMirror" v-html="form.content"></div>
+            @endcan
         @else
-            <div class="ProseMirror" v-html="form.content"></div>
-        @endcan
+            @can('edit-note', [$note])
+                <tip-tap v-model="form.content" note-id="@js($note->id)" :form="form" />
+            @endcan
+        @endif
     </x-splade-form>
     <div
-        class="flex border-t border-fuchsia-400/50 lg:border-none flex-col gap-10 mt-3 pt-4 lg:flex-col lg:fixed lg:left-0 lg:w-80 lg:px-6 py-[1px] lg:top-7 lg:bottom-0 lg:pt-10 lg:overflow-y-auto lg:pb-4">
+        class=" border-t border-fuchsia-400/50 lg:border-none mt-3 pt-4 flex  flex-wrap gap-10 lg:flex-col lg:fixed lg:left-0 lg:w-80 lg:px-6 py-[1px] lg:top-7 lg:bottom-0 lg:pt-10 lg:overflow-y-auto lg:pb-4">
         <x-note.referers :$note />
         <x-note.relatives :$note />
         <x-note.categories inEditor="true" :$note />
     </div>
-    <div
-        class="flex gap-6 flex-wrap mt-8 lg:flex-col lg:fixed lg:right-0 lg:w-80 lg:px-6 py-[1px] lg:top-7 lg:bottom-0 lg:pt-5 lg:overflow-y-auto lg:pb-4">
-        <Link @click.prevent slideover href="{{ route('notes.history') }}" class="text-blue-400">
-        History
-        </Link>
-        <Link @click.prevent slideover href="{{ route('notes.relatives', $note) }}" class="text-blue-400">
-        Toggle references
-        </Link>
-        <Link @click.prevent slideover href="{{ route('categories.index', $note) }}" class="text-blue-400">
-        Toggle Categories
-        </Link>
-        <Link @click.prevent slideover href="{{ route('categories.edit', $note) }}" class="text-blue-400">
-        Edit Categories
-        </Link>
-        <Link @click.prevent slideover href="{{ route('categories.create', $note) }}" class="text-blue-400">
-        New Category
-        </Link>
-        <Link @click.prevent slideover href="{{ route('categories.delete', $note) }}" class="text-blue-400">
-        Delete Categories
-        </Link>
-    </div>
+    @if (!Agent::isPhone())
+        <div
+            class="flex flex-wrap gap-6 mt-8 lg:flex-col lg:fixed lg:right-0 lg:w-80 lg:px-6 py-[1px] lg:top-7 lg:bottom-0 lg:pt-5 lg:overflow-y-auto lg:pb-4">
+            <Link @click.prevent slideover href="{{ route('notes.history') }}" class="text-blue-400">
+            History
+            </Link>
+            <Link @click.prevent slideover href="{{ route('notes.relatives', $note) }}" class="text-blue-400">
+            Toggle references
+            </Link>
+            <Link @click.prevent slideover href="{{ route('categories.index', $note) }}" class="text-blue-400">
+            Toggle Categories
+            </Link>
+            <Link @click.prevent slideover href="{{ route('categories.edit', $note) }}" class="text-blue-400">
+            Edit Categories
+            </Link>
+            <Link @click.prevent slideover href="{{ route('categories.create', $note) }}" class="text-blue-400">
+            New Category
+            </Link>
+            <Link @click.prevent slideover href="{{ route('categories.delete', $note) }}" class="text-blue-400">
+            Delete Categories
+            </Link>
+        </div>
+    @endif
 </div>
