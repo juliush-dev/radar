69051169d7456f09ff3f9b988bc8bec862876403
julius-dev <juliush.dev@gmail.com>
2023-12-27 21:09:38 +0100

    Remove the separator

--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -12,7 +12,7 @@
             <div
                 class="lg:flex flex-col hidden h-[97%] lg:fixed lg:right-0 lg:w-80  py-[1px] lg:top-7 lg:bottom-0 lg:pt-6 lg:overflow-y-auto lg:pb-4 text-blue-400 dark:text-blue-400/30">
                 <div
-                    class="flex flex-col px-6 pb-6 pt-8 gap-4 w-full soft sticky top-0 bg-slate-100/90 dark:bg-slate-950/90 z-10">
+                    class="flex flex-col px-6 pb-6 pt-8 gap-4 w-full soft sticky top-0 bg-slate-100/90 dark:bg-slate-950/90 z-10 border-b border-slate-400/10">
                     <Link @click.prevent slideover href="{{ route('notes.history') }}">
                     History
                     </Link>
@@ -33,9 +33,8 @@
                     </Link>
                 </div>
                 <x-splade-form method="get" action="{{ route('notes.modal') }}" stay background submit-on-change
-                    class="w-full flex-1 px-6 pb-6 rounded-md text-zinc-400 dark:text-zinc-400/30">
+                    class="w-full flex flex-col pt-6 gap-10 flex-1 px-6 pb-6 rounded-md text-zinc-400 dark:text-zinc-400/30">
                     <x-note.referers :$note />
-                    <div class="h-8"></div>
                     <x-note.relatives :$note />
                     <template v-if="form.wasSuccessful">
                         <modal-note :form-response="form.$response" />
